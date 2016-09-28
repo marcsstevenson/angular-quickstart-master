dfef92f94878dc73550425b0793ae4e07aabfbd3
Marc Stevenson <marcsstevenson@hotmail.com>
2016-09-28 19:55:48 +1300

    Init

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
+import { HeroService } from './hero.service';
+import { Hero } from './hero'
+
+@Component({
+    selector: 'my-hero-detail',
+    templateUrl: './app/hero-detail.component.html'
+})
+export class HeroDetailComponent implements OnInit {
+    constructor(
+            private heroService: HeroService,
+            private route: ActivatedRoute,
+            private location: Location
+        ) {}
+
+    hero: Hero;
+
+    ngOnInit(): void {
+        this.route.params.forEach((params: Params) => {
+            let id = +params['id'];
+            this.heroService.getHero(id)
+            .then(hero => this.hero = hero);
+        });
+    }
+    
+    goBack(): void {
+        this.location.back();
+    }
+}
